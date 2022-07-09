c5921a2a8ef66849660a04f9574f69594310138b
Landon <meernik@live.com>
2022-07-09 11:16:32 -0700

    commit

--- Tennis/US Open/src/main.zig
+++ Tennis/US Open/src/main.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
+const swizzler = @import("swizzler.zig");
+const c = @cImport({
+    @cInclude("sys/ioctl.h");
+    @cInclude("linux/spi/spidev.h");
+});
 
-pub const io_mode = .evented;
+//pub const io_mode = .evented;
 
 const Command = enum(u8) {
     COLORS_8BIT = 0x00,
@@ -16,6 +21,75 @@ const Message = struct {
 
 const ClientError = error {
     Disconnected
+};
+
+pub const Server = struct {
+    pixel_buffers: swizzler.PixelBuffers,
+    spi: std.fs.File,
+    allocator: std.mem.Allocator,
+    stream_server: std.net.StreamServer,
+    pub fn init(spi_filename: [] const u8, listen_address: std.net.Address, allocator: std.mem.Allocator) !Server {
+        var stream_server = std.net.StreamServer.init(.{ .reuse_address = true });
+        try stream_server.listen(listen_address);
+        std.log.info("Listening on {}\n", .{stream_server.listen_address});
+        return Server {
+            .pixel_buffers = swizzler.PixelBuffers.init(allocator),
+            .spi = try std.fs.openFileAbsolute(spi_filename, std.fs.File.OpenFlags{.mode = std.fs.File.OpenMode.write_only}),
+            .allocator = allocator,
+            .stream_server = stream_server,
+        };
+    }
+    pub fn run_net(self: *Server) !void {
+        std.log.info("Network loop started",.{});
+        while (true) {
+            var client = Client {
+                .stream = (try self.stream_server.accept()).stream,
+                .allocator = self.allocator,
+            };
+            defer client.deinit();
+            std.log.info("Client Found.\n", .{});
+            while (true) {
+                const message = client.read_message() catch |err| {
+                    std.log.info("Client Lost: {any}\n", .{err});
+                    break;
+                };
+                try self.pixel_buffers.update(message.channel, 0, @ptrCast([]swizzler.Color, message.data));
+                try self.output();
+            }
+        }
+    }
+    pub fn output(self: *Server) !void {
+        const tx = try self.pixel_buffers.output(self.allocator);
+        defer tx.deinit();
+        const ioctl_no = @intCast(u32, c._IOW(c.SPI_IOC_MAGIC, 0, [c.SPI_MSGSIZE(1)]u8));
+        const transfer = [1]c.spi_ioc_transfer{.{
+            .tx_buf = @ptrToInt(&tx.items),
+            .rx_buf = 0,
+            .len = @intCast(u32, @minimum(tx.items.len, 4096)),
+            .delay_usecs = 0,
+            .speed_hz = 6666666,
+            .bits_per_word = 8,
+            .cs_change = 0,
+            .tx_nbits = 0,
+            .rx_nbits = 0,
+            .pad = 0,
+        }};
+        const ret = c.ioctl(self.spi.handle, @bitCast(c_int, ioctl_no), &transfer);
+        if (ret == -1) {
+            std.log.err("IOCTL {d} returned {d}\n",
+                .{
+                    ioctl_no,
+                    ret,
+                }
+            );
+        }
+    }
+    pub fn deinit(self: *Server) void{
+        self.pixel_buffers.deinit();
+        self.spi.deinit();
+        self.lock.deinit();
+        self.stream_server.deinit();
+    }
 };
 
 pub const Client = struct {
@@ -53,36 +127,17 @@ pub const Client = struct {
             .data = data_buf,
         };
     }
-
-    fn handle(self: *Client) !void {
-        defer {
-            self.stream.close();
-        }
-        while (true) {
-            const message = self.read_message() catch |err| {
-                std.log.info("Client Lost: {any}\n", .{err});
-                return;
-            };
-            
-            std.debug.print("Internet!: {any}\n", .{message});
-        }
+    pub fn deinit(self: *Client) void {
+        self.stream.close();
     }
+
 };
 
 pub fn main() anyerror!void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
-    var server = std.net.StreamServer.init(.{ .reuse_address = true });
-    defer server.deinit();
-    try server.listen(std.net.Address.parseIp("127.0.0.1", 42024) catch unreachable);
-    std.log.info("Listening on {}\n", .{server.listen_address});
-    std.debug.print("Alloc: {any}", .{allocator});
-    while(true) {
-        const client = try allocator.create(Client);
-        client.* = Client {
-            .stream = (try server.accept()).stream,
-            .allocator = allocator,
-        };
-        _ = async client.handle();
-    }
+    const addr: std.net.Address = std.net.Address.parseIp("0.0.0.0", 7890) catch unreachable;
+    std.log.info("Attempting to bind {any}", .{addr});
+    var server = try Server.init("/dev/spidev0.0", addr, allocator);
+    try server.run_net();
 }
--- Tennis/US Open/src/spitest.zig
+++ Tennis/US Open/src/spitest.zig
@@ -1,30 +1,42 @@
-const std = @import("std");d
-const spidev = @import("spidev.zig");
-const ioctl = @cImport({
+const std = @import("std");
+const c = @cImport({
     @cInclude("sys/ioctl.h");
+    @cInclude("linux/spi/spidev.h");
 });
-
+const ioctl = c.ioctl;
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
     const stdout = std.io.getStdOut().writer();
     const f = try std.fs.openFileAbsolute("/dev/spidev0.0", std.fs.File.OpenFlags{.mode = std.fs.File.OpenMode.write_only});
-    const ioctl_no = spidev._IOW(spidev.SPI_IOC_MAGIC, 0, [spidev.SPI_MSGSIZE(1)]u8);
-    const tx = [_]u8{0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-    const transfer: spidev.spi_ioc_transfer = .{
+    // zig translate-c doesn't get this macro right
+    const ioctl_no = @intCast(u32, c._IOW(c.SPI_IOC_MAGIC, 0, [c.SPI_MSGSIZE(1)]u8));
+    // 2908 is the max for 1 xfer on raspi
+    // 4096 on jetson
+    const tx_len = 4096;
+    const tx = try allocator.alloc(u8, tx_len);
+    var i: u32 = 0;
+    while (i < tx_len): (i += 1) {
+        tx[i] = 0xA5;
+    }
+    const transfer = [1]c.spi_ioc_transfer{.{
         .tx_buf = @ptrToInt(&tx),
         .rx_buf = 0,
-        .len = 10,
+        .len = tx_len,
         .delay_usecs = 0,
-        .speed_hz = 7000000,
-        .bits_per_word = 0,
+        .speed_hz = 6666666,
+        .bits_per_word = 8,
         .cs_change = 0,
         .tx_nbits = 0,
         .rx_nbits = 0,
         .word_delay_usecs = 0,
         .pad = 0,
-    };
-    const ret = ioctl.ioctl(f.handle, @bitCast(c_int, ioctl_no), &transfer);
+    }};
+    const ret = c.ioctl(f.handle, @bitCast(c_int, ioctl_no), &transfer);
     try stdout.print("IOCTL {d} returned {d}\n",
-        .{ioctl_no, ret}
+        .{
+            ioctl_no,
+            ret,
+        }
     );
 }
